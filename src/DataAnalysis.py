a36202ac2ae983ea8c93fd554218520906c15dc2
POL0307 <paulyungan_97@hotmail.com>
2024-10-12 19:57:39 -0500

    Agrego librerías necesarias para refactorización de EDA

--- src/DataAnalysis.py
+++ src/DataAnalysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 class DataAnalysis: 
     def __init__(self, logger):
         self.logger = logger
