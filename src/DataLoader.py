a2bafc681a6da07f3327c4fdd3af97f7d930eb04
Jorge Chávez Badillo <george_jr11@hotmail.com>
2024-10-19 22:41:56 -0600

    Actualicé el codigo para que corra en cualquier sistema operativo (Windows / MAC)

--- src/DataLoader.py
+++ src/DataLoader.py
@@ -1,7 +1,7 @@
-from ucimlrepo import fetch_ucirepo
-import pandas as pd
 import logging
-
+import os
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
 
 class DataLoaderP:
     def __init__(self) -> None:
@@ -16,15 +16,26 @@ class DataLoaderP:
         data = pd.concat([X, y], axis=1)
         print("Data ready")
 
+        # Get the current working directory (project path)
+        project_path = os.getcwd()
+
+        # Print the project path
+        print(f"Project path: {project_path}")
+
         if save_df: 
             print("Saving dataset...")
-            data.to_csv(r"..\data\raw\raw_dataset2.csv", index=False)
+            # Construct the directory path
+            raw_data_dir = os.path.join(project_path, "data", "raw")
+            # Ensure the directory exists
+            os.makedirs(raw_data_dir, exist_ok=True)
+            # Construct the file path
+            raw_data_path = os.path.join(raw_data_dir, "raw_dataset.csv")
+            # Save the CSV file
+            data.to_csv(raw_data_path, index=False)
+            print(f"Dataset saved to {raw_data_path}")
         else: 
             return data
 
-
 if __name__ == "__main__": 
-    
     loader = DataLoaderP()
     loader.fetch_dataset(save_df=True)
-
